c091046fbb296f2f3ba8b3142772a666f5addcc1
sjparkk <sjparkk0820@naver.com>
2022-01-02 20:42:57 +0900

    feat : TodoForm 추가 내용 App.js에 반영

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import TodoListTemplate from './components/js/TodoListTemplate';
+import Form from './components/js/TodoForm';
 
 class App extends React.Component {
   render() {
     return (
-        <TodoListTemplate>
+        <TodoListTemplate form={<Form />}>
             To-do List를 작성해주세요!!
         </TodoListTemplate>
     );
