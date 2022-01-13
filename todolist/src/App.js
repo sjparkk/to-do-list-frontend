b6b894b6cf1329df2d62322c2fa2fcf527b90237
sjparkk <sjparkk0820@naver.com>
2022-01-13 16:15:42 +0900

    feat : Form 관련 메서드 정의 및 바인딩

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import TodoListTemplate from './components/js/TodoListTemplate';
-import Form from './components/js/Form';
+import Form from './components/js/TodoForm';
 import TodoItemList from './components/js/TodoItemList';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        const id = 2;
+        this.id = 2;
         this.state = {
             input: "",
             todos: [
@@ -14,11 +14,48 @@ class App extends React.Component {
                 { id: 1, content: '리액트를 공부하자1', isComplete: true }
             ]
         }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleCreate = this.handleCreate.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            input: event.target.value
+        });
+    }
+
+    handleCreate() {
+        const { input, todos } = this.state;
+        if (input === "") {
+            alert("오늘 할 일을 입력해주세요!");
+            return;
+        }
+        this.setState({
+            input: "",
+            todos: todos.concat({
+                id: this.id++,
+                content: input,
+                isComplete: false
+            })
+        });
+    }
+
+    handleKeyPress(event) {
+        if (event.key === "Enter") {
+            this.handleCreate();
+        }
     }
 
     render() {
         return (
-            <TodoListTemplate form={<Form />}>
+            <TodoListTemplate form={(
+                <Form
+                    value={this.state.input}
+                    onChange={this.handleChange}
+                    onCreate={this.handleCreate}
+                    onKeyPress={this.handleKeyPress} />
+            )}>
                 <TodoItemList />
             </TodoListTemplate>
         );
