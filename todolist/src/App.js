0ff08c5a0b3054e862ea5b353d29a5190dafb479
sjparkk <sjparkk0820@naver.com>
2022-01-13 15:54:10 +0900

    feat : state 값 설정 추가

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import TodoListTemplate from './components/js/TodoListTemplate';
-import Form from './components/js/TodoForm';
+import Form from './components/js/Form';
 import TodoItemList from './components/js/TodoItemList';
 
 class App extends React.Component {
-  render() {
-    return (
-        <TodoListTemplate form={<Form />}>
-            <TodoItemList />
-        </TodoListTemplate>
-    );
-  }
+    constructor(props) {
+        super(props);
+        const id = 2;
+        this.state = {
+            input: "",
+            todos: [
+                { id: 0, content: '리액트를 공부하자0', isComplete: false },
+                { id: 1, content: '리액트를 공부하자1', isComplete: true }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <TodoListTemplate form={<Form />}>
+                <TodoItemList />
+            </TodoListTemplate>
+        );
+    }
 }
 
 export default App;
